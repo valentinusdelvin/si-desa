506f9b1f7d22cd1adc3738432e88c557e27edd2c
Valentinus Delvin <delvinwicaksono@gmail.com>
2026-05-12 14:19:37 +0700

    chore: update citizen table

--- database/migrations/2026_05_11_163209_create_citizens_table.php
+++ database/migrations/2026_05_11_163209_create_citizens_table.php
@@ -4,22 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('citizens', function (Blueprint $table) {
             $table->id();
+            $table->string('nik', 16)->unique();
+            $table->string('nama');
+            $table->text('alamat');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->date('tanggal_lahir');
+            $table->string('no_hp')->nullable();
+            $table->string('status_perkawinan')->default('Belum Kawin');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('citizens');
