8ba2c7bd8ed0c0576658a32fb01772785a63f69c
Valentinus Delvin <delvinwicaksono@gmail.com>
2026-05-12 14:21:07 +0700

    chore: update mails table

--- database/migrations/2026_05_11_163339_create_mails_table.php
+++ database/migrations/2026_05_11_163339_create_mails_table.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('mails', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('citizen_id')->constrained('citizens')->onDelete('cascade');
+            $table->string('jenis_surat'); // contoh: surat pengantar, surat keterangan domisili
+            $table->text('keperluan');
+            $table->enum('status', ['menunggu', 'diproses', 'selesai', 'ditolak'])->default('menunggu');
+            $table->string('nomor_surat')->nullable(); // diisi admin setelah diproses
+            $table->text('catatan')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('mails');
