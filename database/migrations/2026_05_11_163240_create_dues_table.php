ee999d5c9635dfff5ac7a297611cc4e54361cc37
Valentinus Delvin <delvinwicaksono@gmail.com>
2026-05-12 14:20:40 +0700

    chore: update dues table

--- database/migrations/2026_05_11_163240_create_dues_table.php
+++ database/migrations/2026_05_11_163240_create_dues_table.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('dues', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('citizen_id')->constrained('citizens')->onDelete('cascade');
+            $table->string('keterangan');
+            $table->decimal('nominal', 10, 2);
+            $table->enum('status', ['lunas', 'belum_lunas'])->default('belum_lunas');
+            $table->date('tanggal_bayar')->nullable();
+            $table->string('bulan'); // contoh: "Januari 2025"
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('dues');
