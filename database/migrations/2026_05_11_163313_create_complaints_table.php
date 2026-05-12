93ffaae5e650671160697fa9d1069974b6b8204a
Valentinus Delvin <delvinwicaksono@gmail.com>
2026-05-12 14:20:56 +0700

    chore: update complaints table

--- database/migrations/2026_05_11_163313_create_complaints_table.php
+++ database/migrations/2026_05_11_163313_create_complaints_table.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('complaints', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('citizen_id')->constrained('citizens')->onDelete('cascade');
+            $table->string('judul');
+            $table->text('isi_aduan');
+            $table->enum('kategori', ['infrastruktur', 'keamanan', 'kebersihan', 'sosial', 'lainnya'])->default('lainnya');
+            $table->enum('status', ['menunggu', 'diproses', 'selesai', 'ditolak'])->default('menunggu');
+            $table->text('catatan_admin')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('complaints');
